3c9908a3b2ccce9d081f0967c0807d1986f428fe
Leejunhyuk <wnsgur0813@gmail.com>
2025-06-28 00:36:26 +0900

    헤더 완성

--- src/app/header.jsx
+++ src/app/header.jsx
@@ -10,49 +10,89 @@ const mainMenus = [
     {
         title: '인사 관리',
         submenu: [
-            { label: '소개', href: '/' },
+            { label: '직원 정보 관리', href: '/' },
+            { label: '부서 / 조직 관리', href: '/' },
+            { label: '근태 / 출퇴근 관리', href: '/' },
+            { label: '급여 / 상여 / 수당 관리', href: '/' },
+            { label: '인사평가 / 성과 관리', href: '/' },
+            { label: '휴가 / 연차 관리', href: '/' },
+            { label: '입사 / 퇴사 / 변동 관리', href: '/' },
+            { label: '전자결재(인사)', href: '/' },
         ],
         href: '/',
     },
     {
         title: '상품 / 공급관리',
         submenu: [
-            { label: '운동기관 검색', href: '/' },
+            { label: '상품 목록 / 등록 / 수정', href: '/' },
+            { label: '상품 옵션 / 카테고리 관리', href: '/' },
+            { label: '거래처 관리', href: '/' },
+            { label: '상품 관리', href: '/' },
+            { label: '품절 / 재입고 관리', href: '/' },
+            { label: '상품 신청 / 승인', href: '/' },
         ],
         href: '/',
     },
     {
         title: '재고관리',
         submenu: [
-            { label: '캘린더', href: '/' },
+            { label: '재고 현황', href: '/' },
+            { label: '입고 관리', href: '/' },
+            { label: '출고 관리', href: '/' },
+            { label: '재고 조정', href: '/' },
+            { label: '재고 부족 / 품절 알림', href: '/' },
+            { label: '안전 재고 설정', href: '/' },
+            { label: '재고 회전율 통계', href: '/' },
         ],
         href: '/',
     },
     {
         title: '판매 / 주문관리',
         submenu: [
-            { label: '공지사항 게시판', href: '/' },
+            { label: '주문 목록 / 상세보기', href: '/' },
+            { label: '신규 / 예약 주문 ', href: '/' },
+            { label: '출고 / 배송 관리', href: '/' },
+            { label: '주문 상태 변경', href: '/' },
+            { label: '대량 주문 처리', href: '/' },
+            { label: 'CS / 클레임 처리', href: '/' },
+
         ],
         href: '/',
     },
     {
         title: '정산 / 회계관리',
         submenu: [
-            { label: '공지사항 게시판', href: '/' },
+            { label: '정산 현황', href: '/' },
+            { label: '거래처 / 공급사별 정산', href: '/' },
+            { label: '세금 계산서 / 증빙 관리', href: '/' },
+            { label: '입금 / 지급 관리', href: '/' },
+            { label: '회계 전표 관리', href: '/' },
+            { label: '결산 / 리포트', href: '/' },
+            { label: '예산 실적 관리', href: '/' },
         ],
         href: '/',
     },
     {
         title: '통계',
         submenu: [
-            { label: '공지사항 게시판', href: '/' },
+            { label: '매출 / 매입', href: '/' },
+            { label: '판매 순위', href: '/' },
+            { label: '고객 / 주문 / 공급사', href: '/' },
+            { label: '재고', href: '/' },
+            { label: 'KPI 달성', href: '/' },
+            { label: '맞춤형 리포트', href: '/' },
         ],
         href: '/',
     },
     {
         title: '문서 / 업무 관리',
         submenu: [
-            { label: '공지사항 게시판', href: '/' },
+            { label: '전자결재', href: '/' },
+            { label: '업무별 TO-DO', href: '/' },
+            { label: '문서 관리', href: '/' },
+            { label: '업무 진행 현황', href: '/' },
+            { label: '사내 공지 / 회의록', href: '/' },
+            { label: '첨부파일 / 자료실', href: '/' },
         ],
         href: '/',
     },
@@ -117,7 +157,7 @@ const Header = () => {
             <div className="flex justify-content-between main-back" >
                     <img src='/logo.png' alt ='Logo' width={200} className="logo box-sizing" />
                 {token ? (
-                    <div className="header-text flex align-center justify-right width-fit white-space-nowrap gap_10">
+                    <div className="header-text flex align-center justify-right width-fit white-space-nowrap gap_10 margin-right-4">
                         <div className="header-date-text">{date_time}</div>
                         <div></div>
                         <div><IoMailOutline/></div>
