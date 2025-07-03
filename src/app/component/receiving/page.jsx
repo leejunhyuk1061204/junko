816b36ab527cfc0c9452e0b3a3ba273209f3e1b0
LeeJunHyuk <junhyuk@naver.com>
2025-07-03 15:07:46 +0900

    1

---
+++ src/app/component/receiving/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from "@/app/header";
+
+const ReceivingPage = () => {
+    return (
+        <div>
+            <Header/>
+        </div>
+    );
+};
+
+export default ReceivingPage;
